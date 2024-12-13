03d2509e091a55ca66ff52900189c3bc6b83449b
Mooncheez <62787982+johngao122@users.noreply.github.com>
2024-12-14 00:19:15 +0800

    TailWind Fixed?

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     output: "export",
     basePath: process.env.NODE_ENV === "production" ? "/ais_project" : "",
+    assetPrefix: process.env.NODE_ENV === "production" ? "/ais_project/" : "",
     images: {
         unoptimized: true,
     },
