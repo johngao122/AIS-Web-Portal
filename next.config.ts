dc997480dba2f5e515fdf5565e6409ad9ae774bd
Mooncheez <62787982+johngao122@users.noreply.github.com>
2024-12-14 00:12:24 +0800

    Added CI GH page deployment

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    output: "export",
+    basePath: process.env.NODE_ENV === "production" ? "/ais_project" : "",
+    images: {
+        unoptimized: true,
+    },
     webpack(config: any) {
         config.module.rules.push({
             test: /\.svg$/,
