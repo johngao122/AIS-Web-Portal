1b46b775bf592d09e54fa75e0fa0f1c10e4dee38
Mooncheez <62787982+johngao122@users.noreply.github.com>
2025-01-23 12:49:48 +0800

    vercel and gh pages subbranching

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,17 @@
 import type { NextConfig } from "next";
 import path from "path";
+const isGitHubPages = process.env.GITHUB_PAGES === "true";
 
 const nextConfig: NextConfig = {
-    output: "export",
-    basePath: process.env.NODE_ENV === "production" ? "/AIS-Web-Portal" : "",
-    assetPrefix:
-        process.env.NODE_ENV === "production" ? "/AIS-Web-Portal/" : "",
+    output: "export", // Needed for static exports (GitHub Pages)
+    basePath: isGitHubPages ? "/AIS-Web-Portal" : "", // Set subpath for GitHub Pages
+    assetPrefix: isGitHubPages ? "/AIS-Web-Portal" : "", // Set asset prefix for GitHub Pages
     images: {
-        unoptimized: true,
+        unoptimized: true, // Required for static export when using Next.js Image component
     },
     env: {
         NEXT_PUBLIC_MAPBOX: process.env.NEXT_PUBLIC_MAPBOX,
+        NEXT_PUBLIC_API: process.env.NEXT_PUBLIC_API,
     },
 };
 
